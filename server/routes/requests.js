141ed36b7c92e1363532d019ff910c90251935ba
Ntoils <rredqwerty@gmail.com>
2026-01-24 01:44:31 +0300

    удалил лишнию строчка

--- server/routes/requests.js
+++ server/routes/requests.js
@@ -2,7 +2,6 @@ import {db} from "../config/db.js";
 
 export const requests = (ctx) => {
     ctx.post('/requests', async (req, res) => {
-
         const userResult = await db.query('select * from request')
         return res.status(200).send(userResult.rows[0])
     })
