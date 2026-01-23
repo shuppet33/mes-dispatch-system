e05662aaf1e7c3981a8abbfe0edbb0d8d0044715
Alena <nikishova-alena@mail.ru>
2026-01-23 20:19:22 +0300

    backend: создаст эндпоинт /logout

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -82,7 +82,20 @@ export const authRoute = (ctx) => {
             {expiresIn: '15m'}
         )
 
-        return { accessToken: newAccessToken }
+        return {accessToken: newAccessToken}
+    })
+
+    ctx.post('/logout', async (req, res) => {
+        const refreshToken = req.cookies.refreshToken
+
+        await db.query('UPDATE refresh_token SET is_revoked = true WHERE token = $1 AND is_revoked = false', [refreshToken])
+
+        res.clearCookie('refreshToken', {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'lax',
+            path: '/',
+        });
     })
 
 }
